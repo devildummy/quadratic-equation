2d440bba90ce0edab6e1033ceda72d8ebd7df307
devildummy <cyrok.13@gmail.com>
2018-09-19 02:52:11 +0300

    Just new version. Why answer must be sorted? Whatever.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 module.exports = function solveEquation(equation) {
-  var arr = equation.split(' ');
-  var a=+arr[0], b=+arr[4], c=+arr[8];
-  if (arr[3]==='-'){
+  data = equation.split(' ');
+  let a = data[0],b=data[4],c=data[8],D;
+
+  if(data[3]=='-'){
     b=-b;
-  }
-  if (arr[7]==='-'){
+  }if(data[7]=='-'){
     c=-c;
   }
-  D = (b * b) - (4 * a * c);
-  var x1= Math.round( (-Math.sqrt(D) - b) / (2 * a));
-  var x2= Math.round( (Math.sqrt (D) - b) / (2 * a));
-  var answer=[x1,x2];
-  answer.sort(compareFn)
-  return answer;
-}
-function compareFn(a, b) {
-  return a - b;
-}
+  D=Math.pow(b,2) - (4*a*c);
+  let x1 = (-b - Math.sqrt(D))/(2*a);
+  let x2 = (-b + Math.sqrt(D))/(2*a);
+  return [Math.round(x1),Math.round(x2)].sort(function(a,b){return a-b;});
+};
